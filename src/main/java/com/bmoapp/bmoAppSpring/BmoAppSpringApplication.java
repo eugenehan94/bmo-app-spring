dfa5018f8c54ad41cf2f12b113276fdab5210b36
eugenehan94 <eugene.han94@gmail.com>
2023-11-17 19:02:00 -0500

    bmo spring backend

---
+++ src/main/java/com/bmoapp/bmoAppSpring/BmoAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bmoapp.bmoAppSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmoAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmoAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmoapp/bmoAppSpring/BmoAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmoapp.bmoAppSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmoAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
